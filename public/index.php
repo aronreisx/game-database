f41cbb1a7438e5673970fe60ad6960ef47b5e140
Aron Reis <aron.reis@parceiros.nos.pt>
2021-07-05 12:55:19 +0100

    public/index modified

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
-<?php
-
-echo "hello from Docker"; 
+<!doctype html>
+<style>
+  body { text-align: center; padding: 150px;background-color:blue }
+  h1 { text-align:center;font-size: 25px;color:white  }
+  body { font: 16px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #DC8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+<article>
+    <h1>Welcome to Docker Test Run</h1>
+    <h1>We have installed Nginx , Php-fpm and Mysql</h1>
+</article>
